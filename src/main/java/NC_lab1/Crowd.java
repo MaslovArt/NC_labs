e758a9fabe4c0f30406c12d482e5871741fef534
Maslov <artemiy_maslov@mail.ru>
2017-11-21 17:14:45 +0300

    Добавлен javadoc

--- src/main/java/NC_lab1/Crowd.java
+++ src/main/java/NC_lab1/Crowd.java
@@ -3,7 +3,13 @@ package NC_lab1;
 public class Crowd {
 
     private Person[] people;
+    /**
+     * Кол-во элементов в коллекции
+     */
     private int _count = 0;
+    /**
+     * Емкость коллекции
+     */
     private int capacity = 0;
 
     public Crowd(int capacity) {
@@ -14,12 +20,14 @@ public class Crowd {
     public int Count() {
         return _count;
     }
-
     public Person getAt(int index) {
         return people[index];
     }
 
-
+    /**
+     * Добавление в коллекцию
+     * @param pers новый объект
+     */
     public void add(Person pers) {
         if (_count == capacity) {
             Person[] newArr = new Person[capacity * 2];
@@ -30,6 +38,11 @@ public class Crowd {
         people[_count] = pers;
         _count++;
     }
+
+    /**
+     * Удаление из коллекции по индексу
+     * @param index Индекс
+     */
     public void removeAt(int index){
         Person[] newArr = new Person[capacity - 1];
         System.arraycopy(people, 0, newArr, 0, people.length - index - 1);
@@ -38,16 +51,18 @@ public class Crowd {
         _count--;
         capacity--;
     }
+
+    /**
+     * Удаление из коллекции по id
+     * @param id Айди
+     */
     public void removeById(int id) {
-        int delInd = -1;
         for (int i = 0; i < people.length; i++) {
             if (people[i].getId() == id) {
-                delInd = i;
+                removeAt(i);
                 break;
             }
         }
-        if (delInd != -1)
-            removeAt(delInd);
     }
 
     @Override
